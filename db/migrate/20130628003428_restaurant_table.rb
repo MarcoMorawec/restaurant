10d7d31e8eaea98a6efd8ca2199b044001f74303
marcomorawec <marco.morawec@gmail.com>
2013-06-27 20:40:30 -0400

    fixing the migration table

--- db/migrate/20130628003428_restaurant_table.rb
+++ db/migrate/20130628003428_restaurant_table.rb
@@ -1,16 +1,18 @@
 class RestaurantTable < ActiveRecord::Migration
   def change
-  	t.string "name"
-  	t.text "description"
-  	t.string "street"
-  	t.integer "streetnumber"
-  	t.string "city"
-  	t.integer "zipcode"
-  	t.string "state", :limit => 2
-  	t.string "country"
-  	t.integer "phonenumber"
-
-
+  	create_table :sentences do |t|
+	  	t.string "name"
+	  	t.text "description"
+	  	t.string "street"
+	  	t.integer "streetnumber"
+	  	t.string "city"
+	  	t.integer "zipcode"
+	  	t.string "state", :limit => 2
+	  	t.string "country"
+	  	t.integer "phonenumber"
+	  
+	  t.timestamps
+	end
   end
 
 end
