1fbf10b1252b594d1910dae7b37a5fe537e0eb9b
marcomorawec <marco.morawec@gmail.com>
2013-06-28 07:32:10 -0400

    changed rails app name to reserver so the model restaurant would work

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Restaurant::Application.config.secret_token = '2a7a7e7a559e0e805933e2d1012d8df83197276a2bf8156f8ade3757704b47363a332058934f1c225bd55eec09eed4196ca5926eb5bb6b80d6bc5db3a6584988'
+Reserver::Application.config.secret_token = '2a7a7e7a559e0e805933e2d1012d8df83197276a2bf8156f8ade3757704b47363a332058934f1c225bd55eec09eed4196ca5926eb5bb6b80d6bc5db3a6584988'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Restaurant::Application.routes.draw do
+Reserver::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
