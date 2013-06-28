2f311ebf64b7e8dcb7bad8d215e3bf1b04c9559f
marcomorawec <marco.morawec@gmail.com>
2013-06-27 20:26:24 -0400

    Adding the rails app with a first controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Restaurant::Application.config.secret_token = '2a7a7e7a559e0e805933e2d1012d8df83197276a2bf8156f8ade3757704b47363a332058934f1c225bd55eec09eed4196ca5926eb5bb6b80d6bc5db3a6584988'
